8b8a86debecad943363cabe722cb6bcfd3762279
dbanaszak1 <dominikbanaszak530@gmail.com>
2023-10-25 20:08:13 +0200

    logo

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const NavBar = () => {
   return (
     <nav className='absolute w-5/6 right-0 top-10 h-20 rounded-l-full bg-white z-50 inline-flex items-center border-[1px] border-black'>
-      <div className='inline-flex w-1/2 px-10'>
-        <div>Logo</div>
+      <div className='inline-flex w-1/4 px-10 items-center'>
+        <div className='h-16 w-16 bg-cover' style={{backgroundImage: `url(https://github.com/dbanaszak1/portfolio/blob/main/images/logo-no-background.png?raw=true)`}}></div>
         <div>descr</div>        
       </div>
       <div className='text-2xl w-1/2 text-end px-10'>Links</div>
