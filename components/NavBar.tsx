252d9a1391a212d7789383ba7d52c794987a6c1d
dbanaszak1 <dominikbanaszak530@gmail.com>
2024-01-02 18:23:03 +0100

    edit: nav, contact

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const NavBar = () => {
+
+  const scrollToContent = () => {
+   scrollTo({
+      top: 2500,
+      behavior: "smooth",
+    });
+  }
+
   return (
     <nav className='absolute w-5/6 right-0 top-10 h-20 rounded-l-full bg-white z-50 inline-flex items-center border-[1px] border-black'>
       <div className='inline-flex md:w-1/2 md:px-10 items-center'>
@@ -12,7 +20,7 @@ const NavBar = () => {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
                 </svg>
-            <span className='text-sm md:text-2xl'>Links</span>            
+            <button onClick={scrollToContent} className='text-sm md:text-2xl'>Links</button>            
         </div>
       </div>
     </nav>
