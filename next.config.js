eec5af0c8a4ee70b358401a21546c21ffa7a424d
dbanaszak1 <dominikbanaszak530@gmail.com>
2024-01-02 18:40:26 +0100

    pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    exportPathMap: function () {
+      return {
+        '/': { page: '/' },
+      };
+    },
+    output: 'export',
+  };
+  
+  module.exports = nextConfig;
